d9d4362ccdf76b9e25dc90031f1dc99c5408fe3a
Mike Gerasymenko <mike@gohidoc.com>
2019-10-25 11:50:06 +0200

    Fixed missing header

--- ios/RbmSuggested-Bridging-Header.h
+++ ios/RbmSuggested-Bridging-Header.h
@@ -1,6 +1,6 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-#import <RCTBridgeModule.h>
+#import "RCTBridgeModule.h"
 #import "Matrix.h"
 #import "Matrix+Advanced.h"
